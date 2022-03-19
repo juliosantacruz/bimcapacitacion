86390b68fe56aea58b4929f2fdd5c8c5ff860e5d
Julio Santacruz <aram.jsh1@gmail.com>
2022-03-19 08:37:47 -0700

    inicio configuracion users

--- cursos/views.py
+++ cursos/views.py
@@ -1,9 +1,74 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 from cursos.models import Cursos
 from .models import *
 # Create your views here.
 
+# USERS
+#USER ONLY
+
+def signupuser(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try: 
+                user = User.objects.create_user(
+                    request.POST['username'], 
+                    first_name=request.POST['first_name'], 
+                    last_name=request.POST['last_name'], 
+                    email=request.POST['email'] , 
+                    password=request.POST['password1'])
+                perfil = Member.objects.create(
+                    user = User.objects.all().last(),
+                    profile_picture=request.FILES['img'])
+                
+                user.save()
+                perfil.save()
+                
+                return redirect('home')
+            except IntegrityError:
+                error = 'Tu nombre de usuario fue tomado, intenta con otro'
+                context = {
+                    'error':error,
+                    'form': form,
+                }
+                return render(request, 'members/signupuser.html', context)
+        else:
+            error = 'Error al escribir tu contrasena'
+            context={'error':error,
+                    'form': form,}
+            return render(request, 'members/signupuser.html', context)
+    else:
+        context={
+            'form': form
+        }
+        return render(request, 'members/signupuser.html', context)
+
+
+#USER ONLY
+@login_required
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+
+def loginuser(request):
+    if request.method == 'POST':
+        user = authenticate(request, username=request.POST['username'], password=request.POST['Password'])
+        if user is None:
+            return render(request, 'members/loginuser.html', {'form': AuthenticationForm() , 'error': 'User or password did not match'})
+        else:
+            login(request, user)
+            return redirect('home')
+    else:
+        return render(request, 'members/loginuser.html',  {'form': AuthenticationForm()})
+
+
+
+# PAGINAS
 def home_cursos(request ): #Aqui van todos los cursos
     cursos_list = Cursos.objects.all()
 
